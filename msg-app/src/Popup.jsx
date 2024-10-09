7450f252d7f71cd64de11e7b360754d46c50c03b
yun <ly.yun811@gmail.com>
2024-10-08 21:45:37 -0700

    practice using default props

--- msg-app/src/Popup.jsx
+++ msg-app/src/Popup.jsx
@@ -1,9 +1,20 @@
+import PropTypes from 'prop-types'
+
 function Popup(props) {
     return (
         <div>
             <div>{props.children}</div>
-            <button onClick= { ()=> props.onClose} >Redirect</button>
+            <button onClick= { ()=> props.onClose} >{props.buttonText}</button>
         </div>
     )
+}
+
+Popup.propType = {
+    onClose: PropTypes.func,
+    buttonText: PropTypes.string
+}
+Popup.defaultProps = {
+    onClose: () => {},
+    buttonText: "Default"
 }
 export default Popup;
